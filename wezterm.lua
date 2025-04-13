ebafbc07f486d5124a443fe9f6bed0b78af74a25
yongleyuan <yongle.jack.yuan@gmail.com>
2025-04-12 23:51:33 -0400

    wezterm: change inactive pane appearance

--- wezterm.lua
+++ wezterm.lua
@@ -12,6 +12,7 @@ end
 config.color_scheme = 'nord'
 config.font = wezterm.font('Maple Mono NF')
 config.hide_tab_bar_if_only_one_tab = true
+config.inactive_pane_hsb = { saturation = 0.8, brightness = 0.7 }
 config.window_frame = { font_size = 13 }
 config.window_decorations = 'RESIZE'
 config.font_size = 16
